997b1c26ccf3d931371b07d103c6c0f7285692f6
Cherry Chu <pinker_how.5g@icloud.com>
2026-09-14 00:16:52 +0800

    fix: align Bouncy Castle host dependencies to patched 1.84 (#47)
    * fix: align Bouncy Castle host dependencies to the patched family
    
    * test: reject unresolved dependency graphs in the BC scope check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    dependencies {
+        classpath(platform("org.bouncycastle:bc-jdk18on-bom:1.84"))
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
